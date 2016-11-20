e56d835845cc8b0d1d463f76ae35fcab72e519fd
Frans <franssiswanto@gmail.com>
2016-11-20 15:17:44 +0800

    add cors function

--- run.py
+++ run.py
@@ -1,9 +1,11 @@
 from lib.listener.crawlers_meta import CrawlersMetaListener
+from falcon_cors                import CORS
 import falcon
 
 class Main:
 	def create(self):
-		self.api = falcon.API()
+		self.cors = CORS(allow_all_origins=True)
+		self.api  = falcon.API(middleware=[self.cors.middleware])
 		self.api.add_route("/crawlers_meta", CrawlersMetaListener())
 		return self.api
 
